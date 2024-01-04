c679babde4675ce06366cd6f2265f80238d22aea
muezz <muezz7055@gmail.com>
2024-01-05 03:41:47 +0500

    minor fixes

--- cmd/start.go
+++ cmd/start.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"log"
 	"net"
 	"net/http"
 	"regexp"
@@ -34,7 +33,7 @@ var runCmd = &cobra.Command{
 		r.Use(cors.Default())
 
 		r.POST("/scan", scanHandler)
-		r.POST("/scan-test", scanTestHandler)
+		// r.POST("/scan-test", scanTestHandler)
 
 		r.Run(":7070")
 	},
@@ -78,16 +77,21 @@ func scanHandler(c *gin.Context) {
 	)
 	if err != nil {
 		HandleError(err)
+		c.Writer.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 
 	data, _, err := spClient.From("miner_models").Select("*,manufacturer:manufacturers(*)", "", false).Execute()
 	if err != nil {
 		HandleError(err)
+		c.Writer.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 	var models []MinerModel
 	err = json.Unmarshal(data, &models)
 	if err != nil {
 		HandleError(err)
+		c.Writer.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
@@ -187,24 +191,22 @@ func scanHandler(c *gin.Context) {
 					worker = conf.Pools[0].User
 				}
 				elapsed := 0
-				if len(summary.Summary) > 0 {
-					elapsed = summary.Summary[0].Elapsed
-				}
-				rate5s := 0
-				if len(summary.Summary) > 0 {
-					rate5s = int(summary.Summary[0].Rate_5s)
-				}
-				fanNum := 0
-				if len(stats.Stats) > 0 {
-					fanNum = int(stats.Stats[0].FanNum)
-				}
-				chainNum := 0
-				if len(stats.Stats) > 0 {
-					chainNum = int(stats.Stats[0].ChainNum)
-				}
+				rate5s := 0.0
 				isUnderhashing := false
 				if len(summary.Summary) > 0 {
-					isUnderhashing = (float64(rate5s) / 1000) < (float64(model.HashRate) * 0.8)
+					elapsed = summary.Summary[0].Elapsed
+					rate5s = summary.Summary[0].Rate_5s
+					isUnderhashing = (rate5s / 1000) < (float64(model.HashRate) * 0.8)
+				}
+
+				fanNum := 0
+				chainNum := 0
+				if len(stats.Stats) > 0 {
+					fanNum = stats.Stats[0].FanNum
+					chainNum = stats.Stats[0].ChainNum
+					if fanNum == 0 {
+						fmt.Printf("%v - %v", ip.String(), stats.Stats[0].FanNum)
+					}
 				}
 
 				controller := "N/A"
@@ -256,9 +258,7 @@ func scanHandler(c *gin.Context) {
 					psuFailure,
 				}
 				streamChan <- scannedIp
-				fmt.Printf("%v - %v - %v\n", count.Load(), ip.String(), scannedIp.FanCount)
-				// gCtx.SSEvent("ip-summary", scannedIp)
-				// fmt.Println(scannedIp)
+
 			}(client, ip, models)
 		}
 	}
@@ -316,8 +316,10 @@ func getMinerSummary(
 		return
 	}
 
+	// resBytes, _ := io.ReadAll(res.Body)
+
 	var ipSummary IpSummary
-	err = json.NewDecoder(res.Body).Decode(&ipSummary)
+	json.NewDecoder(res.Body).Decode(&ipSummary)
 	if err != nil {
 		HandleError(err)
 		return
@@ -347,8 +349,10 @@ func getMinerConf(
 		return
 	}
 
+	// resBytes, _ := io.ReadAll(res.Body)
+
 	var ipMinerConf IpMinerConf
-	err = json.NewDecoder(res.Body).Decode(&ipMinerConf)
+	json.NewDecoder(res.Body).Decode(&ipMinerConf)
 	if err != nil {
 		HandleError(err)
 		return
@@ -406,10 +410,13 @@ func getMinerStats(
 		return
 	}
 
+	// resBytes, _ := io.ReadAll(res.Body)
+
 	var ipStats IpStats
-	err = json.NewDecoder(res.Body).Decode(&ipStats)
+	json.NewDecoder(res.Body).Decode(&ipStats)
 	if err != nil {
 		HandleError(err)
+		fmt.Println(err.Error())
 		return
 	}
 
@@ -427,18 +434,18 @@ type ScanApiBody struct {
 }
 
 type ScanApiRes struct {
-	Ip             string `json:"ip"`
-	MinerType      string `json:"miner_type"`
-	Worker         string `json:"worker"`
-	Uptime         int    `json:"uptime"`
-	HashRate       int    `json:"hashrate"`
-	FanCount       int    `json:"fan_count"`
-	HbCount        int    `json:"hb_count"`
-	PowerType      string `json:"power_type"`
-	Controller     string `json:"controller"`
-	IsUnderhashing bool   `json:"is_underhashing"`
-	HashboardType  string `json:"hashboard_type"`
-	PsuFailure     bool   `json:"psu_failure"`
+	Ip             string  `json:"ip"`
+	MinerType      string  `json:"miner_type"`
+	Worker         string  `json:"worker"`
+	Uptime         int     `json:"uptime"`
+	HashRate       float64 `json:"hashrate"`
+	FanCount       int     `json:"fan_count"`
+	HbCount        int     `json:"hb_count"`
+	PowerType      string  `json:"power_type"`
+	Controller     string  `json:"controller"`
+	IsUnderhashing bool    `json:"is_underhashing"`
+	HashboardType  string  `json:"hashboard_type"`
+	PsuFailure     bool    `json:"psu_failure"`
 }
 
 type IpStats struct {
@@ -446,7 +453,7 @@ type IpStats struct {
 		Status     string `json:"STATUS"`
 		When       int    `json:"when"`
 		Msg        string `json:"Msg"`
-		ApiVersion string `json:"api_version"`
+		APIVersion string `json:"api_version"`
 	} `json:"STATUS"`
 	Info struct {
 		MinerVersion string `json:"miner_version"`
@@ -454,33 +461,32 @@ type IpStats struct {
 		Type         string `json:"type"`
 	} `json:"INFO"`
 	Stats []struct {
-		Elapsed   float64 `json:"elapsed"`
-		Rate_5s   float64 `json:"rate_5s"`
-		Rate_30m  float64 `json:"rate_30m"`
+		Elapsed   int     `json:"elapsed"`
+		Rate5S    float64 `json:"rate_5s"`
+		Rate30M   float64 `json:"rate_30m"`
 		RateAvg   float64 `json:"rate_avg"`
 		RateIdeal float64 `json:"rate_ideal"`
 		RateUnit  string  `json:"rate_unit"`
-		ChainNum  float64 `json:"chain_num"`
-		FanNum    float64 `json:"fan_num"`
+		ChainNum  int     `json:"chain_num"`
+		FanNum    int     `json:"fan_num"`
 		Fan       []int   `json:"fan"`
 		HwpTotal  float64 `json:"hwp_total"`
-		MinerMode float64 `json:"miner-mode"`
-		FreqLevel float64 `json:"freq-level"`
+		MinerMode int     `json:"miner-mode"`
+		FreqLevel int     `json:"freq-level"`
 		Chain     []struct {
-			Index        float64     `json:"index"`
-			FreqAvg      float64     `json:"freq_avg"`
-			RateIdeal    float64     `json:"rate_ideal"`
-			RateReal     float64     `json:"rate_real"`
-			AsicNum      float64     `json:"asic_num"`
-			Asic         string      `json:"asic"`
-			TempPic      []float64   `json:"temp_pic"`
-			TempPcb      []float64   `json:"temp_pcb"`
-			TempChip     []float64   `json:"temp_chip"`
-			Hw           float64     `json:"hw"`
-			EepromLoaded bool        `json:"eeprom_loaded"`
-			Sn           string      `json:"sn"`
-			Hwp          float64     `json:"hwp"`
-			Tpl          [][]float64 `json:"tpl"`
+			Index        int     `json:"index"`
+			FreqAvg      int     `json:"freq_avg"`
+			RateIdeal    float64 `json:"rate_ideal"`
+			RateReal     float64 `json:"rate_real"`
+			AsicNum      int     `json:"asic_num"`
+			Asic         string  `json:"asic"`
+			TempPic      []int   `json:"temp_pic"`
+			TempPcb      []int   `json:"temp_pcb"`
+			TempChip     []int   `json:"temp_chip"`
+			Hw           int     `json:"hw"`
+			EepromLoaded bool    `json:"eeprom_loaded"`
+			Sn           string  `json:"sn"`
+			Hwp          float64 `json:"hwp"`
 		} `json:"chain"`
 	} `json:"STATS"`
 }
@@ -518,123 +524,123 @@ type MinerModel struct {
 	} `json:"manufacturer"`
 }
 
-func scanTestHandler(c *gin.Context) {
-	var ipRanges ScanApiBody
-	decoder := json.NewDecoder(c.Request.Body)
+// func scanTestHandler(c *gin.Context) {
+// 	var ipRanges ScanApiBody
+// 	decoder := json.NewDecoder(c.Request.Body)
 
-	if err := decoder.Decode(&ipRanges); err != nil {
-		http.Error(c.Writer, err.Error(), http.StatusBadRequest)
-		c.Writer.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	if len(ipRanges.Ranges) == 0 {
-		http.Error(c.Writer, "No Ranges Provided", http.StatusBadRequest)
-		c.Writer.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	client := &http.Client{
-		Transport: &digest.Transport{
-			Username: "root",
-			Password: "root",
-			// Transport: &http.Transport{
-			// 	Dial: (&net.Dialer{
-			// 		Timeout:   30 * time.Second,
-			// 		KeepAlive: 30 * time.Second,
-			// 	}).Dial,
-			// 	TLSHandshakeTimeout:   3 * time.Second,
-			// 	ResponseHeaderTimeout: 3 * time.Second,
-			// 	ExpectContinueTimeout: 1 * time.Second,
-			// 	MaxIdleConns:          10,
-			// 	IdleConnTimeout:       time.Second * 3,
-			// },
-		},
-		// Timeout: time.Second * 3,
-	}
+// 	if err := decoder.Decode(&ipRanges); err != nil {
+// 		http.Error(c.Writer, err.Error(), http.StatusBadRequest)
+// 		c.Writer.WriteHeader(http.StatusBadRequest)
+// 		return
+// 	}
+// 	if len(ipRanges.Ranges) == 0 {
+// 		http.Error(c.Writer, "No Ranges Provided", http.StatusBadRequest)
+// 		c.Writer.WriteHeader(http.StatusBadRequest)
+// 		return
+// 	}
+// 	client := &http.Client{
+// 		Transport: &digest.Transport{
+// 			Username: "root",
+// 			Password: "root",
+// 			// Transport: &http.Transport{
+// 			// 	Dial: (&net.Dialer{
+// 			// 		Timeout:   30 * time.Second,
+// 			// 		KeepAlive: 30 * time.Second,
+// 			// 	}).Dial,
+// 			// 	TLSHandshakeTimeout:   3 * time.Second,
+// 			// 	ResponseHeaderTimeout: 3 * time.Second,
+// 			// 	ExpectContinueTimeout: 1 * time.Second,
+// 			// 	MaxIdleConns:          10,
+// 			// 	IdleConnTimeout:       time.Second * 3,
+// 			// },
+// 		},
+// 		// Timeout: time.Second * 3,
+// 	}
 
-	spClient, err := supabase.NewClient(
-		"https://conqcdxbczhqszglmwyk.supabase.co",
-		"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNvbnFjZHhiY3pocXN6Z2xtd3lrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzk4NjM0NDQsImV4cCI6MTk5NTQzOTQ0NH0.LNi12BRKMOOmqW396mjgm_wgJp79U-Ie994EyLlfxnc",
-		nil,
-	)
-	if err != nil {
-		HandleError(err)
-	}
+// 	spClient, err := supabase.NewClient(
+// 		"https://conqcdxbczhqszglmwyk.supabase.co",
+// 		"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNvbnFjZHhiY3pocXN6Z2xtd3lrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzk4NjM0NDQsImV4cCI6MTk5NTQzOTQ0NH0.LNi12BRKMOOmqW396mjgm_wgJp79U-Ie994EyLlfxnc",
+// 		nil,
+// 	)
+// 	if err != nil {
+// 		HandleError(err)
+// 	}
 
-	data, _, err := spClient.From("miner_models").Select("*,manufacturer:manufacturers(*)", "", false).Execute()
-	if err != nil {
-		HandleError(err)
-	}
-	var models []MinerModel
-	err = json.Unmarshal(data, &models)
-	if err != nil {
-		HandleError(err)
-		return
-	}
+// 	data, _, err := spClient.From("miner_models").Select("*,manufacturer:manufacturers(*)", "", false).Execute()
+// 	if err != nil {
+// 		HandleError(err)
+// 	}
+// 	var models []MinerModel
+// 	err = json.Unmarshal(data, &models)
+// 	if err != nil {
+// 		HandleError(err)
+// 		return
+// 	}
 
-	wg := sync.WaitGroup{}
-	ctx := context.TODO()
-	count := atomic.Int32{}
-	sem := semaphore.NewWeighted(200)
+// 	wg := sync.WaitGroup{}
+// 	ctx := context.TODO()
+// 	count := atomic.Int32{}
+// 	sem := semaphore.NewWeighted(200)
 
-	streamChan := make(chan ScanApiRes)
+// 	streamChan := make(chan ScanApiRes)
 
-	for ri := 0; ri < len(ipRanges.Ranges); ri++ {
-		ipRange := ipRanges.Ranges[ri]
-		for ip := ipRange.Start; !ip.Equal(ipRange.End); ip = incrementIP(ip) {
-			wg.Add(1)
+// 	for ri := 0; ri < len(ipRanges.Ranges); ri++ {
+// 		ipRange := ipRanges.Ranges[ri]
+// 		for ip := ipRange.Start; !ip.Equal(ipRange.End); ip = incrementIP(ip) {
+// 			wg.Add(1)
 
-			go func(client *http.Client, ip net.IP, models []MinerModel) {
-				sem.Acquire(ctx, 1)
-				defer wg.Done()
-				defer sem.Release(1)
-				defer count.Add(1)
-				s := time.Now()
+// 			go func(client *http.Client, ip net.IP, models []MinerModel) {
+// 				sem.Acquire(ctx, 1)
+// 				defer wg.Done()
+// 				defer sem.Release(1)
+// 				defer count.Add(1)
+// 				s := time.Now()
 
-				// _, err := client.Head(fmt.Sprintf("http://%s", ip))
-				_, err := client.Head("https://example.com/")
-				// time.Sleep(time.Second * 3)
-				if err != nil {
-					log.Println(err)
-				}
-				fmt.Printf("%v - %v - %v\n", count.Load(), ip.String(), time.Since(s))
+// 				// _, err := client.Head(fmt.Sprintf("http://%s", ip))
+// 				_, err := client.Head("https://example.com/")
+// 				// time.Sleep(time.Second * 3)
+// 				if err != nil {
+// 					log.Println(err)
+// 				}
+// 				fmt.Printf("%v - %v - %v\n", count.Load(), ip.String(), time.Since(s))
 
-				scannedIp := ScanApiRes{
-					ip.String(),
-					"",
-					"",
-					0,
-					0,
-					0,
-					0,
-					"",
-					"",
-					true,
-					"",
-					true,
-				}
-				streamChan <- scannedIp
+// 				scannedIp := ScanApiRes{
+// 					ip.String(),
+// 					"",
+// 					"",
+// 					0,
+// 					0,
+// 					0,
+// 					0,
+// 					"",
+// 					"",
+// 					true,
+// 					"",
+// 					true,
+// 				}
+// 				streamChan <- scannedIp
 
-			}(client, ip, models)
-		}
-	}
-	c.Header("content-type", "application/json")
-	c.Writer.WriteHeader(http.StatusOK)
-	go func(s chan ScanApiRes) {
-		for item := range s {
-			c.SSEvent("ip-summary", item)
-		}
-		// c.Stream(func(w io.Writer) bool {
-		// 	if msg, ok := <-s; ok {
-		// 		c.Writer.Flush()
-		// 		c.SSEvent("ip-summary", msg)
-		// 		return true
-		// 	}
-		// 	return false
-		// })
+// 			}(client, ip, models)
+// 		}
+// 	}
+// 	c.Header("content-type", "application/json")
+// 	c.Writer.WriteHeader(http.StatusOK)
+// 	go func(s chan ScanApiRes) {
+// 		for item := range s {
+// 			c.SSEvent("ip-summary", item)
+// 		}
+// 		// c.Stream(func(w io.Writer) bool {
+// 		// 	if msg, ok := <-s; ok {
+// 		// 		c.Writer.Flush()
+// 		// 		c.SSEvent("ip-summary", msg)
+// 		// 		return true
+// 		// 	}
+// 		// 	return false
+// 		// })
 
-	}(streamChan)
+// 	}(streamChan)
 
-	wg.Wait()
-	close(streamChan)
+// 	wg.Wait()
+// 	close(streamChan)
 
-}
+// }
